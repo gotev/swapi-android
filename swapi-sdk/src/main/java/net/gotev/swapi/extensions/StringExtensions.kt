e85cb05f83bc2fc94e2bd11931217b8626f327ce
Alex Gotev <alex@gotev.net>
2020-04-15 08:56:43 +0200

    get page number differently when using swapi mirror

--- swapi-sdk/src/main/java/net/gotev/swapi/extensions/StringExtensions.kt
+++ swapi-sdk/src/main/java/net/gotev/swapi/extensions/StringExtensions.kt
@@ -1,6 +1,8 @@
 package net.gotev.swapi.extensions
 
+import net.gotev.swapi.SwapiOrigin
 import net.gotev.swapi.models.Manufacturer
+import net.gotev.swapi.swapiClient
 import java.util.Locale
 
 fun String.asCSVList() = split(",").map { it.trim() }
@@ -9,7 +11,11 @@ fun String.asLintedLowercaseCSVList() = asCSVList()
     .map { it.split("/") }
     .flatten() // theed palace space vessel engineering corps/nubia star drives
 
-fun String.pageNumber() = split("=").getOrNull(1)?.toInt()
+fun String.pageNumber() = if (swapiClient is SwapiOrigin) {
+    split("=").getOrNull(1)?.toInt()
+} else {
+    split("page").getOrNull(1)?.replace(".json", "")?.toInt()
+}
 
 internal fun String.lintManufacturer() = when (val linted = replace(".", "")) {
     "incom" -> Manufacturer.IncomCorporation.rawValue
